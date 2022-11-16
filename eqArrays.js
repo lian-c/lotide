3823a56be314bb90c082022497b40e2eb8966ecf
lian-c <lian.chan@telus.com>
2022-11-15 19:03:45 -0800

    Added function that will compare two arrays if they're same type and value or not

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(first, sec) {
+  for (let i = 0; i <= first.length; i++) {
+    if (first[i] !== sec[i]) {
+      return false;
+    }
+  } return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // should be falsey
