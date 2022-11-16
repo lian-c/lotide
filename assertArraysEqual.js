01525f11f2a483f88e5f23d3c4c5d9bd30957541
lian-c <lian.chan@telus.com>
2022-11-15 19:19:11 -0800

    Created an assertArraysEqual function to log if it's equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+// eqArrays will compare 2 arrays and if they're all the same it'll return true
+//assertArraysEqual will log if it's true or false
+
+const eqArrays = function(first, sec) {
+  for (let i = 0; i <= first.length; i++) {
+    if (first[i] !== sec[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(first, sec) {
+  if (eqArrays(first,sec) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${first} is same as ${sec}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${first} does not equal to ${sec}`);
+  }
+};
+
+assertArraysEqual([1,2,3],[1,2,3]);
