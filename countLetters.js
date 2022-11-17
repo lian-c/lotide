5fd3f984d76a8f0d1c9c9d15252c19c9cc3b27cd
lian-c <lian.chan@telus.com>
2022-11-16 17:19:49 -0800

    fixed formatting

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,18 +6,18 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const countLetters = function (strings) {
-  let results = {}
-  for (let str of strings){
-   if (str === " ") continue; // skips if there's a space
-    if (results[str]) { 
+const countLetters = function(strings) {
+  let results = {};
+  for (let str of strings) {
+    if (str === " ") continue; // skips if there's a space
+    if (results[str]) {
       results[str] += 1;
     } else {
       results[str] = 1;
     }
-   
-  }  console.log(results)
+
+  } console.log(results);
 
 };
 
-countLetters("lighthouse in the house")
+countLetters("lighthouse in the house");
