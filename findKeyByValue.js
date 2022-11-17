922a708057d4bf7010c9b44876c5a47ed259d100
lian-c <lian.chan@telus.com>
2022-11-16 19:36:55 -0800

    created a function that finds the key based on the value within an object

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) { // we are entering the object and value to obtain the key
+  for (let key of Object.keys(object)) {  //Object.keys give you an array of the keys in the object,the for..of loop gets the individual key
+    if (object[key] === value) { //object[key] will give the value so object["comedy"] would be "Brooklyn Nine-Nine" and making sure it is equal
+      console.log(key);
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine");
+findKeyByValue(bestTVShowsByGenre, "The Wire");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
