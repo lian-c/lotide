fd40ad812255f8d8e42bbf95493a39ebdba243cd
lian-c <lian.chan@telus.com>
2022-11-16 17:12:00 -0800

    counts each letter and gives us the number

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (strings) {
+  let results = {}
+  for (let str of strings){
+   if (str === " ") continue; // skips if there's a space
+    if (results[str]) { 
+      results[str] += 1;
+    } else {
+      results[str] = 1;
+    }
+   
+  }  console.log(results)
+
+};
+
+countLetters("lighthouse in the house")
