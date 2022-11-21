a1d463b23a24a728bff7214ac92aaeaae79a78d4
lian-c <lian.chan@telus.com>
2022-11-21 14:10:15 -0800

    Changed the eqArrays into the function file and testing file, refactored so easier to understand

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(first, sec) {
   for (let i = 0; i <= first.length; i++) {
     if (first[i] !== sec[i]) {
@@ -13,6 +5,4 @@ const eqArrays = function(first, sec) {
     }
   } return true;
 };
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3",4]),false); // should be falsey
+module.exports = eqArrays;
