b745af620b29e0cb6487c69cc8e0265a486740ee
lian-c <lian.chan@telus.com>
2022-11-21 16:52:24 -0800

    changed console.log to return within the function

--- without.js
+++ without.js
@@ -1,21 +1,8 @@
 // eqArrays will compare 2 arrays and if they're all the same it'll return true
 //assertArraysEqual will log if it's true or false
 
-const eqArrays = function (first, sec) {
-  for (let i = 0; i <= first.length; i++) {
-    if (first[i] !== sec[i]) {
-      return false;
-    }
-  } return true;
-};
-
-const assertArraysEqual = function (first, sec) {
-  if (eqArrays(first, sec) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${first} is same as ${sec}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${first} does not equal to ${sec}`);
-  }
-};
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
 
 //assertArraysEqual([1, 2, 3], [1, 2, 3]);
 //only push when it loops over all the items to remove and it does not match
@@ -25,8 +12,10 @@ const without = function (source, itemsToRemove) {
   for (let i = 0; i < itemsToRemove.length; i++) { 
     itemsToKeep = itemsToKeep.filter((item) => item !== itemsToRemove[i])
 
-  }     console.log(itemsToKeep)
+  }     return(itemsToKeep)
 };
+
+//test  below
 without([1, 2, 3, 1], [1, "2"]) // => [2,3]
 
 without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
