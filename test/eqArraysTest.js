aadb6e3c5f1f71a62f1d6b834f0048d6e74d63cd
lian-c <lian.chan@telus.com>
2022-11-21 16:47:02 -0800

    changed eqArrays refactored with Chai and Mocha functions

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,20 @@
-const assertEqual = require('../assertEqual')
+// const assertEqual = require('../assertEqual')
 const eqArrays = require('../eqArrays')
+const assert = require('chai').assert
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3",4]),false); // should be falsey
+describe("## eqAssert", ()=> {
+  it("[1,2,3] matches [1,2,3] should return true", ()=> {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+  });
+  
+  it("[1,2,3] does not match [1,2,3,4] should return false", ()=> {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3,4]), false); 
+
+  });
+
+});
+
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3",4]),false); // should be falsey
